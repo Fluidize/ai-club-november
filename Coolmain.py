40122fa1a6af5f7f3ff362a2a10c15b91839c5e7
CoolDude113 <JaxonCm113@gmail.com>
2024-11-16 18:07:53 -0600

    clean up
    all columns but it needs some work...

---
+++ Coolmain.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import sklearn
+import numpy as np
+
+traindf=pd.read_csv("train.csv",index_col=0)
+
+
+X=traindf.drop(["SalePrice"],inplace=False,axis=1)
+y=traindf["SalePrice"]
+
+
+x=cleanup(X)
+
+
+print(x.head())
+
+def cleanup(X):
+    for i in range(len(X)):
+        data=X[i]
+        
+        if (data[0]!=int):
+            sets=set(data)
+            lib={}
+            count=0
+
+            for i in set():
+                lib.update({i:count})
+                count+=1
+            
+            for i in range(len(data)):
+                data[i]=lib[data[i]]
+            X[i]=data
+
+    return X
+
