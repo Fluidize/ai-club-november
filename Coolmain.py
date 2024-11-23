07bd90035462840caecbf3a49fef0026a2672a5b
CoolDude113 <JaxonCm113@gmail.com>
2024-11-23 16:44:48 -0600

    debugs
    I debugged the code involving finding the columns and cleaning the columns

--- Coolmain.py
+++ Coolmain.py
@@ -7,26 +7,33 @@ traindf=pd.read_csv("train.csv",index_col=0)
 
 X=traindf.drop(["SalePrice"],inplace=False,axis=1)
 y=traindf["SalePrice"]
+cols=X.columns.tolist()
+print(cols)
 
-
-
-def cleanup(X):
-    for i in range(len(X)):
-        data=X[i]
-        
-        if (data[0]!=int):
+def cleanup(X,column):
+    for i in range(len(column)):
+        data=X[column[i]].tolist()
+    
+        if (type(data[0])!=int):
             sets=set(data)
             lib={}
             count=0
 
-            for i in set():
-                lib.update({i:count})
+
+            for j in sets:
+                lib.update({j:count})
                 count+=1
             
-            for i in range(len(data)):
-                data[i]=lib[data[i]]
-            X[i]=data
+
+
+
+            for k in range(len(data)):
+                data[k]=lib[data[k]]
+            X[column[i]]=data
 
     return X
 
-x=cleanup(X)
+X=cleanup(X,cols)
+print(X.head)
+
+
