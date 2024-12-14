d682cc9977562adb505171ee0259af029137c408
coderguy134 <142461272+coderguy134@users.noreply.github.com>
2024-12-14 17:47:41 -0600

    Commented out unnecessary things

--- Coolmain.py
+++ Coolmain.py
@@ -6,7 +6,6 @@ from sklearn.preprocessing import StandardScaler
 
 traindf=pd.read_csv("train.csv",index_col=0)
 
-
 X=traindf.drop(["SalePrice"],inplace=False,axis=1)
 y=traindf["SalePrice"]
 cols=X.columns.tolist()
@@ -21,14 +20,10 @@ def cleanup(X,column):
             lib={}
             count=0
 
-
             for j in sets:
                 lib.update({j:count})
                 count+=1
-            
-
-
-
+        
             for k in range(len(data)):
                 data[k]=lib[data[k]]
             X[column[i]]=data
@@ -40,15 +35,15 @@ print(X.head)
 
 """
 run=MLP.fit_transform(X)"""
-MLP=MLPRegressor()
+MLP=MLPRegressor(learning_rate_init=0.01, max_iter=1461)
 
-param_dist = {"hidden_layer_sizes": [(100,)],
-              "random_state": [None],
-              "max_iter": [200]}
+#param_dist = {"hidden_layer_sizes": range(1, 1000,),
+#              "random_state": range(1, 100),
+#              "max_iter": range(1, 500)}
 
-random_search = RandomizedSearchCV(MLP, param_distributions=param_dist,
-                                   n_iter=20, cv=5)
-random_search.fit(X, y)
+#random_search = RandomizedSearchCV(MLP, param_distributions=param_dist,
+#                                   n_iter=20, cv=5)
+MLP.fit(X, y)
 
 testdf=pd.read_csv("test.csv",index_col=0)
 Xtest=testdf
@@ -58,10 +53,10 @@ Xtest=cleanup(Xtest,testcols)
 print(Xtest.head)
 
 
-prediction=pd.DataFrame(random_search.predict(Xtest), columns = ["SalePrice"])
+prediction=pd.DataFrame(MLP.predict(Xtest), columns = ["SalePrice"])
 
 prediction.index.name = "Id"
 
 prediction.index+=1461
 
-prediction.to_csv("housePrediction2.csv")
+prediction.to_csv("housePrediction1.csv")
