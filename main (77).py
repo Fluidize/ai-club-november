b378bc4998e9f33a5688e3e699fa43643d1ef9a0
Gr00vingM0nkey <144180370+Gr00vingM0nkey@users.noreply.github.com>
2024-12-21 16:14:00 -0600

    Add files via upload

---
+++ main (77).py
@@ -0,0 +1,47 @@
+import pandas as pd
+from sklearn.neural_network import MLPRegressor
+#0.05 , 0.005, 0.00051
+model=MLPRegressor(alpha=0.00051,max_iter=1500)
+
+traindf=pd.read_csv("train.csv",index_col=0)
+
+X=traindf.drop(["SalePrice"],inplace=False,axis=1)
+y=traindf["SalePrice"]
+
+def cleanup(X,column):
+    for i in range(len(column)):
+        data=X[column[i]].tolist()
+    
+        if (type(data[0])!=int):
+            sets=set(data)
+            lib={}
+            count=0
+
+            for j in sets:
+                lib.update({j:count})
+                count+=1
+
+            for k in range(len(data)):
+                data[k]=lib[data[k]]
+            X[column[i]]=data
+
+    return X
+
+cols=X.columns.tolist()
+X=cleanup(X,cols)
+
+print('TRAINING')
+model.fit(X,y)
+
+testdf=pd.read_csv("test.csv",index_col=0)
+
+testcols=testdf.columns.tolist()
+testdf=cleanup(testdf,testcols)
+
+prediction=pd.DataFrame(model.predict(testdf), columns = ["SalePrice"])
+
+prediction.index.name = "Id"
+
+prediction.index+=1461
+
+prediction.to_csv("prediction.csv")
